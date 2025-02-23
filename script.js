bb4d7f53eccce022fb8fd15a0a04c784dd0fad34
GasMan <gastondana627@gmail.com>
2025-02-23 02:22:06 -0600

    Cursor & toggle features

--- script.js
+++ script.js
@@ -151,3 +151,17 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   }
 });
+
+// Add dark/light themes
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+  }
+  else {
+      document.documentElement.setAttribute('data-theme', 'light');
+  }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
